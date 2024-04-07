75381378890d798bd185578d36b8ae2b6bc4d0bb
Gerard <gerard@bee-haven.com>
2024-04-06 23:06:57 -0400

    completed version of file sorter

--- File Sorter/file_sorter.py
+++ File Sorter/file_sorter.py
@@ -2,16 +2,17 @@ from os import scandir, rename
 from os.path import splitext, exists, join
 from shutil import move
 from time import sleep
-import logging
+
 
 #folder to check
-source_folder = "c:\Users\gerar\Downloads"
-#destination folders
-image_destination ="C:\Users\gerar\OneDrive\Desktop\Images"
-docs_destination = "C:\Users\gerar\OneDrive\Desktop\Documents"
-pdf_destination = "C:\Users\gerar\OneDrive\Desktop\PDFs"
-exe_destination = "C:\Users\gerar\OneDrive\Desktop\executables"
-video_destination = "C:\Users\gerar\OneDrive\Desktop\Videos"
+source_folder = "c:\\Users\\gerar\\Downloads"
+
+#destination folders already exist
+image_destination ="C:\\Users\\gerar\\OneDrive\\Desktop\\Images"
+docs_destination = "C:\\Users\\gerar\\OneDrive\\Desktop\\Documents"
+pdf_destination = "C:\\Users\\gerar\\OneDrive\\Desktop\\PDFs"
+exe_destination = "C:\\Users\\gerar\\OneDrive\\Desktop\\executables"
+video_destination = "C:\\Users\\gerar\\OneDrive\\Desktop\\Videos"
 
 #associating file extensions with file types
 image_ext = [".png", ".jpg", ".jpeg"]
@@ -20,22 +21,66 @@ exe_ext = [".exe"]
 pdf_ext = [".pdf"]
 video_ext = [".mp4"]
 
-#file finder calls checker functions
+
+#file iterator calls subsequent functions
 def file_iterator():
-    with scandir(source_folder) as file_names:
-        for file_name in file_names:
+    with scandir(source_folder) as source_files:
+        for current_file in source_files:
+            file_name = current_file.name
+            check_doc(current_file, file_name)
+            check_exe(current_file, file_name)
+            check_image(current_file, file_name)
+            check_pdf(current_file, file_name)
+            check_video(current_file, file_name)
 
 
-#file checker functions check
-def check_image(entry, name):
-    for x in image_ext
+#to make sure files not overwritten
+def make_unique(destination, name_ext):
+    name, ext = splitext(name_ext)
+    counter = 1
+    
+    while exists(f"{destination}/{name_ext}"):
+        name_ext = f"{name}({str(count)}){ext}"
+        count += 1
 
-def check_doc():
-    pass
+    return name_ext
 
-def check_exe():
-    pass
-def check_pdf():
-    pass
-def check_video():
-    pass
+#moves files without overwriting
+def move_file(destination, file_path, file_name):
+    if exists(f"{destination}/{file_name}"):
+        unique_name = make_unique(destination, file_name)
+        oldName = join(destination, file_name)
+        newName = join(destination, unique_name)
+        rename(oldName, newName)
+    move(file_path, destination)
+
+#file checker functions check which extension it has, then sorts accordingly
+def check_image(a_file, file_name):
+    for ext in image_ext:
+        if(file_name.endswith(ext)):
+            move_file(image_destination, a_file, file_name)  
+def check_doc(a_file, file_name):
+    for ext in docs_ext:
+         if(file_name.endswith(ext)):
+            move_file(docs_destination, a_file, file_name)
+def check_exe(a_file, file_name):
+    for ext in exe_ext:
+         if(file_name.endswith(ext)):
+            move_file(exe_destination, a_file, file_name)
+def check_pdf(a_file, file_name):
+    for ext in pdf_ext:
+         if(file_name.endswith(ext)):
+            move_file(pdf_destination, a_file, file_name)
+def check_video(a_file, file_name):
+    for ext in video_ext:
+         if(file_name.endswith(ext)):
+            move_file(video_destination, a_file, file_name)
+
+
+
+
+#running the functions to sort the files
+file_iterator()
+
+
+
